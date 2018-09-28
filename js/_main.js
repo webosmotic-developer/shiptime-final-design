aa500e0cfe1dcf69f95da30428496f53eca52665
WHIZZSERVER\whizz <whizzwebwork@gmail.com>
2018-09-28 17:48:57 +0530

    - Form lable floting - quote-ship changes + payment tab new design

--- js/_main.js
+++ js/_main.js
@@ -27,6 +27,16 @@ $(function() {
     
 feather.replace()
 
+$('body').on('click', function (e) {
+    $('[data-toggle="popover"]').each(function () {
+        //the 'is' for buttons that trigger popups
+        //the 'has' for icons within a button that triggers a popup
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+            $(this).popover('hide');
+        }
+    });
+});
+
 $("._xs-collapsible").click(function() {
     if ($("._xs-collapsible").hasClass("active")) {
         $("._xs-collapsible").removeClass('active');
